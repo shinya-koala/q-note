6b707cbb34108de8fd43d39e6ac5a85a0ecdec2d
shinya <sinya4649@gmail.com>
2023-11-07 12:11:30 +0900

    first commit.

---
+++ src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+    <Head>
+      <link rel="manifest" href="/manifest.json" />
+    </Head>
+    <Component {...pageProps} />
+    </>
+  )
+}
