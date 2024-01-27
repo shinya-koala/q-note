d96b4397b788a4cd279602b8cd0fdde813aaac31
shinya <sinya4649@gmail.com>
2024-01-27 22:35:58 +0900

    add db update code.

--- src/app/page/page.js
+++ src/app/page/page.js
@@ -3,18 +3,29 @@ import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import styles from "./page.module.scss";
 import { db } from "../components/firebase";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, doc, updateDoc } from "firebase/firestore";
+
+const TEXT_DATA_FIELD = "memoText";
 
 function Index() {
   const [text, setText] = useState([]);
+  const [inputText, setInputText] = useState("");
 
   useEffect(() => {
+    // DB取得
     const dbPosts = async () => {
       const DocsData = await getDocs(collection(db, "memo_data"));
       setText(DocsData.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
     };
     dbPosts();
   }, []);
+
+  const updateDB = async () => {
+    const DocsData = doc(db, "memo_data", "A7zRyxcU8ySgPLv9lWZw");
+    await updateDoc(DocsData, {
+      [TEXT_DATA_FIELD]: inputText,
+    });
+  };
 
   return (
     <>
@@ -27,13 +38,14 @@ function Index() {
           </ul>
           <div className={styles["debug-buttons"]}>
             <button>読み込み</button>
-            <button>保存</button>
+            <button onClick={updateDB}>保存</button>
           </div>
         </div>
         <div className={styles["input-area"]}>
           <textarea
             className={styles["input-text"]}
-            defaultValue={text[0].test}
+            defaultValue={text[0] && text[0][TEXT_DATA_FIELD]}
+            onChange={(e) => setInputText(e.target.value)}
           ></textarea>
         </div>
       </div>
