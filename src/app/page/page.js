7d511bc9afdf1ffecbf6813cad9c9073bf9b5d7e
shinya <sinya4649@gmail.com>
2024-01-28 19:11:51 +0900

    Add loading and save shortcutKey.

--- src/app/page/page.js
+++ src/app/page/page.js
@@ -1,31 +1,49 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import Link from "next/link";
 import styles from "./page.module.scss";
 import { db } from "../components/firebase";
-import { collection, getDocs, doc, updateDoc } from "firebase/firestore";
+import { collection, getDoc, doc, updateDoc } from "firebase/firestore";
 
 const TEXT_DATA_FIELD = "memoText";
 
 function Index() {
-  const [text, setText] = useState([]);
   const [inputText, setInputText] = useState("");
 
+  const GetDocFromKey = async (key) => {
+    const docRef = doc(db, "memo_data", key);
+    const docSnap = await getDoc(docRef);
+    const result = docSnap.data();
+    setInputText(result?.[TEXT_DATA_FIELD]);
+  };
+
   useEffect(() => {
-    // DB取得
-    const dbPosts = async () => {
-      const DocsData = await getDocs(collection(db, "memo_data"));
-      setText(DocsData.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    };
-    dbPosts();
+    GetDocFromKey("A7zRyxcU8ySgPLv9lWZw");
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const updateDB = async () => {
+  // アップデート保存
+  const updateDB = useCallback(async () => {
     const DocsData = doc(db, "memo_data", "A7zRyxcU8ySgPLv9lWZw");
     await updateDoc(DocsData, {
       [TEXT_DATA_FIELD]: inputText,
     });
-  };
+  }, [inputText]);
+
+  useEffect(() => {
+    // ctrl + sで保存
+    const handleSaveShortcut = async (e) => {
+      if (e.ctrlKey && e.key === "s") {
+        e.preventDefault();
+        await updateDB();
+        console.log("保存しました。");
+      }
+    };
+    document.addEventListener("keydown", handleSaveShortcut);
+    return () => {
+      document.removeEventListener("keydown", handleSaveShortcut);
+    };
+  }, [updateDB]);
 
   return (
     <>
@@ -37,15 +55,18 @@ function Index() {
             </li>
           </ul>
           <div className={styles["debug-buttons"]}>
-            <button>読み込み</button>
+            <button onClick={() => GetDocFromKey("A7zRyxcU8ySgPLv9lWZw")}>
+              読み込み
+            </button>
             <button onClick={updateDB}>保存</button>
           </div>
         </div>
         <div className={styles["input-area"]}>
           <textarea
             className={styles["input-text"]}
-            defaultValue={text[0] && text[0][TEXT_DATA_FIELD]}
+            // defaultValue={inputText}
             onChange={(e) => setInputText(e.target.value)}
+            value={inputText}
           ></textarea>
         </div>
       </div>
