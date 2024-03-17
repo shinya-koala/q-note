62ba7b100e7df80763935572161ed1136f19b41a
shinya <sinya4649@gmail.com>
2024-03-17 15:57:46 +0900

    add responsive code.

--- src/app/components/MainScreen.js
+++ src/app/components/MainScreen.js
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { useCallback, useEffect, useRef, useState } from "react";
+import { useMediaQuery } from "@mui/material";
 import Link from "next/link";
 import { styled } from "@mui/system";
 import MarkdownIt from "markdown-it";
@@ -19,6 +20,8 @@ import {
   Typography,
 } from "@mui/material";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 import Head from "next/head";
 import Image from "next/image";
 
@@ -76,6 +79,8 @@ const md = new MarkdownIt();
 function MainScreen({ id }) {
   const [inputText, setInputText] = useState("");
   const [openEditer, setOpenEditer] = useState(false);
+  const [showSidebar, setShowSidebar] = useState(true); // Drawerのスライド
+  const [showTextarea, setShowTextarea] = useState(false); // テキストエリアの表示領域100%
   const inputRef = useRef();
 
   const GetDocFromKey = async (key) => {
@@ -187,196 +192,286 @@ function MainScreen({ id }) {
           width: "100%",
           height: "calc(100vh - 64px)",
           overflow: "hidden",
+          transition: "transform 0.3s ease-in-out",
+          //  transform: showSidebar ? "translateX(0)" : "translateX(-300px)",
         }}
       >
-        <StyledDrawer
-          variant="permanent"
-          PaperProps={{
-            style: {
-              position: "relative",
-              overflow: "hidden auto",
-              display: "block",
-              backgroundColor: "#E8E8E8",
-              padding: "40px 0",
-            },
-          }}
-        >
-          <Box
-            className="qr-code"
-            sx={{
-              width: "150px",
-              height: "150px",
-              margin: "0 auto",
-              backgroundColor: "#DDD",
+        {showSidebar && (
+          <StyledDrawer
+            variant="persistent"
+            open={showSidebar}
+            PaperProps={{
+              style: {
+                position: "relative",
+                overflow: "hidden auto",
+                backgroundColor: "#E8E8E8",
+                padding: "40px 0",
+                "@media (max-width: 768px)": {
+                  display: showSidebar ? "block" : "",
+                  position: "fixed",
+                  top: "64px",
+                },
+              },
             }}
           >
-            {/*QRコードが入ってくる*/}
-          </Box>
-          {/* メモリスト */}
-          <StyledList>
-            <StyledListItemSec>
-              <StyledListItemTextSec secondary="メモリスト" />
-            </StyledListItemSec>
-            <StyledListItem>
-              <StyledListItemText
-                primary={<StyledTypography>Menu Item 1</StyledTypography>}
-              />
-            </StyledListItem>
-            <StyledListItem>
-              <StyledListItemText
-                primary={
-                  <StyledTypography>
-                    Menu Item 2Menu Item 2Menu Item 2Menu Item 2Menu Item 2Menu
-                    Item 2Menu Item 2Menu Item 2Menu Item 2
-                  </StyledTypography>
-                }
-              />
-            </StyledListItem>
-            <StyledListItem>
-              <Link href="./pages/memoId/1">
-                <StyledListItemText primary="Menu Item 2" />
-              </Link>
-            </StyledListItem>
-          </StyledList>
-          {/* 履歴 */}
-          <StyledList>
-            <StyledListItemSec>
-              <StyledListItemTextSec secondary="履歴" />
-            </StyledListItemSec>
-            <StyledListItem>
-              <StyledListItemText
-                primary={<StyledTypography>Menu Item 1</StyledTypography>}
-              />
-            </StyledListItem>
-            <StyledListItem>
-              <StyledListItemText
-                primary={
-                  <StyledTypography>
-                    Menu Item 2Menu Item 2Menu Item 2Menu Item 2Menu Item 2Menu
-                    Item 2Menu Item 2Menu Item 2Menu Item 2
-                  </StyledTypography>
-                }
-              />
-            </StyledListItem>
-            <StyledListItem>
-              <Link href="./pages/memoId/1">
-                <StyledListItemText primary="Menu Item 2" />
-              </Link>
-            </StyledListItem>
-          </StyledList>
-          {/* ブックマーク */}
-          <StyledList>
-            <StyledListItemSec>
-              <StyledListItemTextSec secondary="ブックマーク" />
-            </StyledListItemSec>
-            <StyledListItem>
-              <StyledListItemText
-                primary={<StyledTypography>Menu Item 1</StyledTypography>}
-              />
-            </StyledListItem>
-            <StyledListItem>
-              <StyledListItemText
-                primary={
-                  <StyledTypography>
-                    Menu Item 2Menu Item 2Menu Item 2Menu Item 2Menu Item 2Menu
-                    Item 2Menu Item 2Menu Item 2Menu Item 2
-                  </StyledTypography>
-                }
-              />
-            </StyledListItem>
-            <StyledListItem>
-              <Link href="./pages/memoId/1">
-                <StyledListItemText primary="Menu Item 2" />
-              </Link>
-            </StyledListItem>
-          </StyledList>
-          　　　　　　　　　
-          {/* デバッグボタン */}
-          <Box
-            className="buttons-area"
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              width: "100%",
-              /*maxWidth: "144px",*/
-              pr: 3,
-              pl: 3,
-              textAlign: "center",
-            }}
-          >
-            <Button
-              onClick={() => GetDocFromKey("A7zRyxcU8ySgPLv9lWZw")}
-              variant="contained"
+            <Box
+              className="qr-code"
               sx={{
-                mb: 2,
-                width: "100%",
-                maxWidth: "104px",
+                width: "150px",
+                height: "150px",
+                margin: "0 auto",
+                backgroundColor: "#DDD",
               }}
             >
-              読み込み
-            </Button>
-            <Button
-              onClick={updateDB}
-              variant="contained"
+              {/*QRコードが入ってくる*/}
+            </Box>
+            {/* メモリスト */}
+            <StyledList>
+              <StyledListItemSec>
+                <StyledListItemTextSec secondary="メモリスト" />
+              </StyledListItemSec>
+              <StyledListItem>
+                <StyledListItemText
+                  primary={<StyledTypography>Menu Item 1</StyledTypography>}
+                />
+              </StyledListItem>
+              <StyledListItem>
+                <StyledListItemText
+                  primary={
+                    <StyledTypography>
+                      Menu Item 2Menu Item 2Menu Item 2Menu Item 2Menu Item
+                      2Menu Item 2Menu Item 2Menu Item 2Menu Item 2
+                    </StyledTypography>
+                  }
+                />
+              </StyledListItem>
+              <StyledListItem>
+                <Link href="./pages/memoId/1">
+                  <StyledListItemText primary="Menu Item 2" />
+                </Link>
+              </StyledListItem>
+            </StyledList>
+            {/* 履歴 */}
+            <StyledList>
+              <StyledListItemSec>
+                <StyledListItemTextSec secondary="履歴" />
+              </StyledListItemSec>
+              <StyledListItem>
+                <StyledListItemText
+                  primary={<StyledTypography>Menu Item 1</StyledTypography>}
+                />
+              </StyledListItem>
+              <StyledListItem>
+                <StyledListItemText
+                  primary={
+                    <StyledTypography>
+                      Menu Item 2Menu Item 2Menu Item 2Menu Item 2Menu Item
+                      2Menu Item 2Menu Item 2Menu Item 2Menu Item 2
+                    </StyledTypography>
+                  }
+                />
+              </StyledListItem>
+              <StyledListItem>
+                <Link href="./pages/memoId/1">
+                  <StyledListItemText primary="Menu Item 2" />
+                </Link>
+              </StyledListItem>
+            </StyledList>
+            {/* ブックマーク */}
+            <StyledList>
+              <StyledListItemSec>
+                <StyledListItemTextSec secondary="ブックマーク" />
+              </StyledListItemSec>
+              <StyledListItem>
+                <StyledListItemText
+                  primary={<StyledTypography>Menu Item 1</StyledTypography>}
+                />
+              </StyledListItem>
+              <StyledListItem>
+                <StyledListItemText
+                  primary={
+                    <StyledTypography>
+                      Menu Item 2Menu Item 2Menu Item 2Menu Item 2Menu Item
+                      2Menu Item 2Menu Item 2Menu Item 2Menu Item 2
+                    </StyledTypography>
+                  }
+                />
+              </StyledListItem>
+              <StyledListItem>
+                <Link href="./pages/memoId/1">
+                  <StyledListItemText primary="Menu Item 2" />
+                </Link>
+              </StyledListItem>
+            </StyledList>
+            　　　　　　　　　
+            {/* デバッグボタン */}
+            <Box
+              className="buttons-area"
               sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
                 width: "100%",
-                maxWidth: "104px",
+                pr: 3,
+                pl: 3,
+                textAlign: "center",
               }}
             >
-              保存
-            </Button>
-          </Box>
-        </StyledDrawer>
-
+              <Button
+                onClick={() => GetDocFromKey("A7zRyxcU8ySgPLv9lWZw")}
+                variant="contained"
+                sx={{
+                  mb: 2,
+                  width: "100%",
+                  maxWidth: "104px",
+                }}
+              >
+                読み込み
+              </Button>
+              <Button
+                onClick={updateDB}
+                variant="contained"
+                sx={{
+                  width: "100%",
+                  maxWidth: "104px",
+                }}
+              >
+                保存
+              </Button>
+            </Box>
+            <Box
+              className="slider-button"
+              sx={{
+                display: "none",
+                position: "absolute",
+                top: 0,
+                left: 0,
+                zIndex: 1,
+                "@media (max-width: 768px)": {
+                  display: "block", // 768px以下で表示
+                },
+              }}
+            >
+              <Button
+                variant="contained"
+                onClick={() => {
+                  setShowSidebar(!showSidebar);
+                  setShowTextarea(true); // Drawerを閉じる時にテキストエリア表示フラグを立てる
+                }}
+              >
+                <ArrowBackIosNewIcon />
+              </Button>
+            </Box>
+          </StyledDrawer>
+        )}
         {/* テキストエリア */}
         <Box
           className="textArea-wrap"
           sx={{
-            width: "100%",
+            position: "relative",
+            width: showSidebar ? "calc(100% - 300px)" : "100%",
             height: "100vh",
+            position: "relative",
+            transition: "width 0.3s ease-in-out",
+            // テキストエリアの拡大表示
+            ...(showTextarea && {
+              width: "100%",
+              left: 0, // 必要に応じて調整
+              top: 0, // 必要に応じて調整
+              zIndex: 1000, // 上に重ねる場合
+            }),
+            "@media (max-width: 768px)": {
+              marginTop: "36px",
+            },
           }}
         >
-          <Box className="textArea-inner">
-            {/* // フォーカスが外れた時に入力と表示を入れ替える */}
-            <TextareaAutosize
-              aria-label="empty textarea"
-              placeholder="テキストを入力してください。"
-              style={{
-                width: "100%",
+          {/* drawerのスライドボタン
+          <Box
+            className="slider-buttom"
+            sx={{
+              display: "none",
+              position: "absolute",
+              top: 0,
+              left: 0,
+              zIndex: 1,
+              "@media (max-width: 768px)": {
+                display: "block", // 768px以下で表示
+              },
+            }}
+          >
+            <Button
+              variant={showSidebar ? "outlined" : "contained"}
+              onClick={() => setShowSidebar(!showSidebar)}
+              sx={{
+                minWidth: "32px",
                 height: "100vh",
-                display: openEditer ? "block" : "none",
+                padding: "68px 8px",
+              }}
+            >
+              {showSidebar ? (
+                <ArrowBackIosNewIcon />
+              ) : (
+                <ArrowForwardIosIcon style={{ color: "#FFF" }} />
+              )}
+            </Button>
+          </Box> */}
+          {/* // フォーカスが外れた時に入力と表示を入れ替える */}
+          <TextareaAutosize
+            aria-label="empty textarea"
+            placeholder="テキストを入力してください。"
+            style={{
+              width: "100%",
+              height: "100vh",
+              display: openEditer ? "block" : "none",
+              overflow: "visible scroll",
+            }}
+            onChange={(e) => setInputText(e.target.value)}
+            value={inputText}
+            ref={inputRef}
+            onBlur={() => {
+              setOpenEditer(false);
+            }}
+            open={openEditer}
+          />
+          {!openEditer && (
+            <Box
+              sx={{
+                width: "auto",
+                height: "100vh",
+                padding: "0 24px",
                 overflow: "visible scroll",
+                display: openEditer ? "none" : "block",
               }}
-              onChange={(e) => setInputText(e.target.value)}
-              value={inputText}
-              ref={inputRef}
-              onBlur={() => {
-                setOpenEditer(false);
+              dangerouslySetInnerHTML={{ __html: htmlText }}
+              onClick={() => {
+                setOpenEditer(true);
+                if (inputRef.current) {
+                  setTimeout(() => {
+                    inputRef.current.focus();
+                  }, "10");
+                }
               }}
-              open={openEditer}
             />
-            {!openEditer && (
-              <Box
-                sx={{
-                  width: "auto",
-                  height: "100vh",
-                  padding: "0 24px",
-                  overflow: "visible scroll",
-                  display: openEditer ? "none" : "block",
-                }}
-                dangerouslySetInnerHTML={{ __html: htmlText }}
-                onClick={() => {
-                  setOpenEditer(true);
-                  if (inputRef.current) {
-                    setTimeout(() => {
-                      inputRef.current.focus();
-                    }, "10");
-                  }
-                }}
-              />
-            )}
-          </Box>
+          )}
+        </Box>
+        {/* menuを閉じるスイッチ */}
+        <Box
+          className="close-menu-button"
+          onClick={() => setShowSidebar(!showSidebar)}
+          sx={{
+            display: "none",
+            position: "absolute",
+            left: 0,
+            ransition: "transform 0.5s ease-in-out",
+            transform: showSidebar ? "translateX(-300px)" : "translateX(0)",
+            "@media (max-width: 768px)": {
+              display: "block", // 768px以下で表示
+            },
+          }}
+        >
+          <Button variant="contained">
+            <ArrowForwardIosIcon style={{ color: "#FFF" }} />
+          </Button>
         </Box>
       </Box>
     </>
