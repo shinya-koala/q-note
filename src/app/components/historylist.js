8f2df1c70dcebe6205339120402718a4d1891e81
shinya <sinya4649@gmail.com>
2024-03-20 23:43:36 +0900

    fix import

--- src/app/components/historylist.js
+++ src/app/components/historylist.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useCallback } from "react";
 import { styled } from "@mui/system";
 import { ListItem } from "@mui/material";
-import { HistoryListItem } from "./historylistitem";
+import { HistoryListItem } from "./historyListItem";
 
 const StyledListItem = styled(ListItem)(({ theme }) => ({}));
 
